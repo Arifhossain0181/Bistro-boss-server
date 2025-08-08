7f8deab74c38132375468e4fc9c0eff130237de8
Arifhossain0181 <arifhossain01816047075@gmail.com>
2025-08-08 23:56:16 +0600

    your message

--- index.js
+++ index.js
@@ -59,6 +59,10 @@ async function run() {
       }
     });
     //carts collection
+    app.get('/carts',async(req,res) =>{
+      const result = await cartCollection.find().toArray()
+      res.send(result)
+    })
     app.post('/carts' ,async(req,res ) =>{
       const cartitem = req.body;
       const result = await cartCollection.insertOne(cartitem);
